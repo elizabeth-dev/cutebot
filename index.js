13d76c87691cccdb965bc18b4f024315a4a0e2ed
Alba Mendez <alba.mendez@basetis.com>
2018-10-06 21:54:14 +0200

    Necessary fix
    It's not fair you're also beautiful (>^__^)>

--- index.js
+++ index.js
@@ -9,5 +9,7 @@ const bot = botgram(process.env.TELEGRAM_TOKEN, { agent: agent })
 bot.all((msg, reply) => {
 	if (msg.from.id === parseInt(process.env.USER_ID)) {
 		reply.text('Et dius Alba i ets una gateta molt bonica')
+	} else if (msg.from.id === parseInt(process.env.CREATOR_ID)) {
+		reply.text('Et dius Eli i ets una gateta molt dolça')
 	}
 })
