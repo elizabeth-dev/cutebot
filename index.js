3bb879f62e796927ae456023af6d2e703c074290
Elizabeth Martín Campos <eli@zabeth.es>
2018-10-06 20:51:14 +0200

    First release

---
+++ index.js
@@ -0,0 +1,13 @@
+'use strict'
+
+require('dotenv').config()
+const https = require('https')
+const agent = new https.Agent({ keepAlive: true, maxFreeSockets: 5 })
+const botgram = require('botgram')
+const bot = botgram(process.env.TELEGRAM_TOKEN, { agent: agent })
+
+bot.all((msg, reply) => {
+	if (msg.from.id === parseInt(process.env.USER_ID)) {
+		reply.text('Et dius Alba i ets molt bonica')
+	}
+})
