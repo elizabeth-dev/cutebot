2ea4619b62edab8da5ca6e3e10c182bd5c2cdf24
Javier Revillas <jrevillas@massivedynamic.io>
2018-10-06 23:59:51 +0200

    Avoid hitting environment for every request

--- index.js
+++ index.js
@@ -6,10 +6,13 @@ const agent = new https.Agent({ keepAlive: true, maxFreeSockets: 5 })
 const botgram = require('botgram')
 const bot = botgram(process.env.TELEGRAM_TOKEN, { agent: agent })
 
+const CREATOR_ID = parseInt(process.env.CREATOR_ID)
+const USER_ID = parseInt(process.env.USER_ID)
+
 bot.all((msg, reply) => {
-	if (msg.from.id === parseInt(process.env.USER_ID)) {
+	if (msg.from.id === USER_ID) {
 		reply.text('Et dius Alba i ets una gateta molt bonica')
-	} else if (msg.from.id === parseInt(process.env.CREATOR_ID)) {
+	} else if (msg.from.id === CREATOR_ID) {
 		reply.text('Et dius Eli i ets una gateta molt dolça')
 	}
 })
